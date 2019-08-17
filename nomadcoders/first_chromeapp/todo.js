3a5a71d3ddc1bf9ecf58283f7095612336d72be9
박진영 <xiaru.jy@dktechin.com>
2019-08-17 15:36:19 +0900

    크롬앱_todo리스트 삭제 기능 적용

--- nomadcoders/first_chromeapp/todo.js
+++ nomadcoders/first_chromeapp/todo.js
@@ -3,7 +3,19 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");//등록된 li를 담을 ul
 
 const TODOS_LS = "toDos";//로컬스토리지에 저장될 todo리스트의 키값 설정
-const toDos = [];//todo 리스트 배열을 저장할 변수 선언
+
+let toDos = [];//todo 리스트 배열을 저장할 변수 선언, const면 내용변경이 안되니까 let으로 선언한다.
+
+function deleteToDo(event){//todo리스트를 지우는 함수 선언
+    const btn = event.target;//눌린 버튼을 변수에 할당
+    const li = btn.parentNode;//눌린버튼의 부모 li를 변수에 할당
+    toDoList.removeChild(li);//todo리스트에서 눌린 버튼 부모li를 지움
+    const CleanToDos = toDos.filter(function(toDo){//todo에 대해 하나하나 비교해서 찾음
+        return toDo.id !== parseInt(li.id);//눌린 버튼의 부모 id가 todo리스트의 id가 같지 않은 것들만 찾아 반환
+    });
+    toDos = CleanToDos;//찾아낸 li들을 todo리스트 배열변수에 할당
+    saveToDos();//로컬스토리지에 투두리스트를 저장시키는 함수 실행
+}
 
 function saveToDos(){//로컬스토리지에 투두리스트를 저장시키는 함수 선언
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));//로컬스토리지 toDos키에 todo리스트의 json 키, 밸류값을 담은 배열로 저장시킴
@@ -14,6 +26,7 @@ function paintToDo(text){//todo등록 함수 선언
     const span = document.createElement("span");//span 만드는 변수 선언
     const newId = toDos.length + 1;//li에 부여될 id 값= todo리스트 배열의 현재 길이에 1을 추가한 값으로 다음 생성 시킬 리스트의 id를 부여함
     delBtn.innerText = "❌";//버튼을 만드는 변수의 텍스트로 x 모양 이모지 적용
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;//span의 텍스트로 인풋 벨류로 넘어온 값을 넘김
     li.appendChild(span);//li에 span을 넣어줌
     li.appendChild(delBtn);//li에 del버튼을 넣어줌
